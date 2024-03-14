318b12e838bf820859de04c6bf669567566a696c
AlbinaKost <143324469+AlbinaKost@users.noreply.github.com>
2024-03-14 16:18:21 +0200

    update hw2

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import Description from './components/Description/Description';
+import Feedback from './components/Feedback/Feedback';
+import Options from './components/Options/Options';
+import Notification from './components/Notification/Notification';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [types, setTypes] = useState(() => {
+    const savedTypes = window.localStorage.getItem('saved-types');
+    if (savedTypes) {
+      return JSON.parse(savedTypes);
+    }
+    return {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem('saved-types', JSON.stringify(types));
+  }, [types]);
+
+  const updateFeedback = feedbackType => {
+    setTypes({
+      ...types,
+      [feedbackType]: types[feedbackType] + 1,
+    });
+  };
+
+  const totalFeedback = types.good + types.neutral + types.bad;
+
+  const updateTotalFeedback = () => {
+    setTypes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  const positiveStatistics = Math.round(
+    ((types.good + types.neutral) / totalFeedback) * 100
+  );
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Description />
+      <Options
+        updateFeedback={updateFeedback}
+        totalFeedback={totalFeedback}
+        updateTotalFeedback={updateTotalFeedback}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback
+          types={types}
+          totalFeedback={totalFeedback}
+          positiveStatistics={positiveStatistics}
+        />
+      ) : (
+        <Notification />
+      )}
     </>
-  )
+  );
+    
+  
 }
 
 export default App
